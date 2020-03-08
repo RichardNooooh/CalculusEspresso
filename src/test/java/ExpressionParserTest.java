497537be26081e87609c71eaecea14241ba97b7f
Ice <richard.jun.noh@gmail.com>
2020-03-08 01:36:21 -0600

    Add substituteMultiCharOp tests for log char
    The following changes have been made:
    -3 more tests added for sqrt to check behavior with binary operators
    -9 tests added for log, similar to sqrt
    -Empty tests for der, int, and grad
    -Refactor chars to utilize Operator char values instead of hard code

--- src/test/java/ExpressionParserTest.java
+++ src/test/java/ExpressionParserTest.java
@@ -4,7 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//NOTE if I want to utilize the @Tag, watch https://www.youtube.com/watch?v=0m-vtBB66cI&list=PLqq-6Pq4lTTa4ad5JISViSb2FVG8Vwa4o&index=25
 @DisplayName("When running ExpressionParser")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ExpressionParserTest
@@ -22,6 +22,11 @@ public class ExpressionParserTest
 	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 	class substituteMultiCharOpTest
 	{
+		final char SQRT = Operator.SQUARE_ROOT.getChar();
+		final char LOG = Operator.LOGARITHM.getChar();
+		final char DER = Operator.DERIVATIVE.getChar();
+		final char INT = Operator.INTEGRAL.getChar();
+
 		Method method;
 
 		@BeforeEach
@@ -45,14 +50,55 @@ public class ExpressionParserTest
 		public void sqrtTest()
 		{
 			assertAll("should return substituted char in string with preserved spacing",
-					() -> assertEquals((char) 208 + "2", invokeExpr("sqrt2")),
-					() -> assertEquals((char) 208 + "(2)", invokeExpr("sqrt(2)")),
-					() -> assertEquals((char) 208 + " 2", invokeExpr("sqrt 2")),
-					() -> assertEquals((char) 208 + " (2)", invokeExpr("sqrt (2)")),
-					() -> assertEquals("x" + (char) 208 + " 24x", invokeExpr("xsqrt 24x")),
-					() -> assertEquals("x" + (char) 208 + "24x", invokeExpr("xsqrt24x")),
-					() -> assertEquals("x " + (char) 208 + "24x", invokeExpr("x sqrt24x"))
+					() -> assertEquals(SQRT + "2", invokeExpr("sqrt2")),
+					() -> assertEquals(SQRT + "(2)", invokeExpr("sqrt(2)")),
+					() -> assertEquals(SQRT + " 2", invokeExpr("sqrt 2")),
+					() -> assertEquals(SQRT + " (2)", invokeExpr("sqrt (2)")),
+					() -> assertEquals("x" + SQRT + " 24x", invokeExpr("xsqrt 24x")),
+					() -> assertEquals("x" + SQRT + "24x", invokeExpr("xsqrt24x")),
+					() -> assertEquals("x " + SQRT + "24x", invokeExpr("x sqrt24x")),
+					() -> assertEquals("x +" + SQRT + "24x", invokeExpr("x +sqrt24x")),
+					() -> assertEquals("x * 3 +" + SQRT + "24+x", invokeExpr("x * 3 +sqrt24+x"))
 			);
+		}
+
+		@Test
+		@DisplayName((char) 209 + " instead of log")
+		public void logTest()
+		{
+			assertAll("should return substituted char in string with preserved spacing",
+					() -> assertEquals(LOG + "2", invokeExpr("log2")),
+					() -> assertEquals(LOG + "(2)", invokeExpr("log(2)")),
+					() -> assertEquals(LOG + " 2", invokeExpr("log 2")),
+					() -> assertEquals(LOG + " (2)", invokeExpr("log (2)")),
+					() -> assertEquals("x" + LOG + " 24x", invokeExpr("xlog 24x")),
+					() -> assertEquals("x" + LOG + "24x", invokeExpr("xlog24x")),
+					() -> assertEquals("x " + LOG + "24x", invokeExpr("x log24x")),
+					() -> assertEquals("x +" + LOG + "24x", invokeExpr("x +log24x")),
+					() -> assertEquals("x * 3 +" + LOG + "24+x", invokeExpr("x * 3 +log24+x"))
+			);
+		}
+
+		@Test
+		@DisplayName((char) 213 + " instead of der")
+		public void derTest()
+		{
+
+		}
+
+		@Test
+		@DisplayName((char) 214 + " instead of int")
+		public void intTest()
+		{
+
+		}
+
+		@Disabled
+		@Test
+		@DisplayName("")
+		public void gradTest()
+		{
+
 		}
 
 	}
