c2b4e6bb9878c90301a41d540c1b10e0639530d9
Ice <richard.jun.noh@gmail.com>
2020-03-17 21:53:03 -0500

    Add evaluate method to ExpressionTree.java

--- src/main/java/ExpressionTree.java
+++ src/main/java/ExpressionTree.java
@@ -1,15 +1,26 @@
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Stack;
 
+/**
+ * This is the primary calculator tree
+ */
 public class ExpressionTree
 {
 	Node root;
 
+	public ExpressionTree(String expression)
+	{
+		//TODO isPostfix boolean assumed to be true for now.
+		ExpressionParser parser = new ExpressionParser(expression, true);
+		LinkedList<Node> tokens = parser.getTokens();
+		setTree(tokens);
+	}
+
 	public ExpressionTree(LinkedList<Node> tokens)
 	{
 		setTree(tokens);
-		System.out.println("Done");
 	}
 
 	private void setTree(LinkedList<Node> tokens)
@@ -31,6 +42,9 @@ public class ExpressionTree
 		root = values.pop();
 	}
 
-
+	public double evaluate(HashMap<Character, Double> variableValues)
+	{
+		return root.eval(variableValues);
+	}
 
 }
