d7d1b0612978e8c68a6f4c8b27be0ae5f918f9a7
Ice <richard.jun.noh@gmail.com>
2020-04-25 23:21:16 -0500

    Add documentation and start of the derivative calc

--- src/main/java/CalculusNode.java
+++ src/main/java/CalculusNode.java
@@ -1,3 +1,4 @@
+
 import java.util.Map;
 
 /**
@@ -5,24 +6,49 @@ import java.util.Map;
  */
 public class CalculusNode extends OperatorNode
 {
-	public CalculusNode(Operator bOperator)
+	/**
+	 * 'h' is the del-x value of the calculus operations
+	 */
+	private final int h = 0;
+
+	/**
+	 * Calculus Node Constructor
+	 * @param cOperator is a calculus operator.
+	 */
+	public CalculusNode(Operator cOperator)
 	{
-		this.operator = bOperator;
+		this.operator = cOperator;
 	}
 
+	/**
+	 * Evaluates the current node and its subtrees.
+	 * @param env is the set of all variables and their corresponding values
+	 * @return the result of the operation
+	 */
 	@Override
 	protected double eval(Map<Character, Double> env)
 	{
 		double val = right.eval(env);
+//		double input = left.eval(env);
 
 		switch(operator)
 		{
 			case DERIVATIVE:
 				return val;
-			case INTEGRAL:
-				return val;
+//			case INTEGRAL:
+//				return val;
 			default:
 				return 0;
 		}
 	}
+
+	/**
+	 * Calculates the derivative of the function to its right based on the value on its left.
+	 * @return the derivative
+	 */
+	private double evaluateDerivative()
+	{
+		return 0;
+	}
+
 }
