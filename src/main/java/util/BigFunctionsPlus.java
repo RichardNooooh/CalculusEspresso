9b79f28dec1b3191b599d7f936542190868a4b3f
Ice <richard.jun.noh@gmail.com>
2020-06-29 22:55:53 -0500

    Add log() and root() methods to BigFunctionsPlus

--- src/main/java/util/BigFunctionsPlus.java
+++ src/main/java/util/BigFunctionsPlus.java
@@ -20,5 +20,31 @@ public class BigFunctionsPlus
             return BigDecimal.ZERO;
         return BigFunctions.exp(BigFunctions.ln(x, SCALE).multiply(y), SCALE);
     }
+
+    /**
+     * Compute the value log_n(x)
+     * @param x the value of x
+     * @param n the value of the log base
+     * @return the value of log_n(x)
+     */
+    public static BigDecimal log(BigDecimal x, BigDecimal n)
+    {
+        if (x.doubleValue() == 0 || n.doubleValue() == 0)
+            throw new IllegalArgumentException("The log() method cannot have a non-positive base or x");
+        return BigFunctions.ln(x, SCALE).divide(BigFunctions.ln(n, SCALE));
+    }
+
+    /**
+     * Compute the value of the nth root(x)
+     * @param x the value of x
+     * @param n the value of the radical index
+     * @return the value of root_n(x)
+     */
+    public static BigDecimal root(BigDecimal x, BigDecimal n)
+    {
+        if (n.doubleValue() == 0)
+            throw new IllegalArgumentException("The root() method cannot have a 0 as its index.");
+        return pow(x, BigDecimal.ONE.divide(n));
+    }
 }
 
