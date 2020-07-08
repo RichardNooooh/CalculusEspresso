5c8af28bb1f27c1aa867bc529c23bc6f4cbdd2cc
Ice <richard.jun.noh@gmail.com>
2020-07-07 21:21:06 -0500

    Add comment to package statement
    The Apache license requires that any change made to this class be
    documented. Since the only thing I've changed is the package statement,
    I've added that comment for documentation's sake.

--- src/main/java/org/ideaman/calculus_espresso/util/tareknaj/BigFunctions.java
+++ src/main/java/org/ideaman/calculus_espresso/util/tareknaj/BigFunctions.java
@@ -1,4 +1,6 @@
+//This line is the only thing changed to this class.
 package org.ideaman.calculus_espresso.util.tareknaj;
+
 /*Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
